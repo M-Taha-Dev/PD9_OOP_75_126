29699dc3782118d4383c45a56808f730ea1fb99d
Muhammad Khuzaima Umair <75787251+mkhuzaima@users.noreply.github.com>
2021-04-24 16:33:38 +0500

    Completed the task 2
    This program takes input and displays each character at a new line.

---
+++ task2/src/task2/Driver.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author M_Khuzaima
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner obj = new Scanner(System.in);
+        
+        System.out.println("Please enter a sentence: ");
+        String input = obj.nextLine();
+        
+        System.out.println("Your sentence printed vertically is: ");
+        System.out.println(displayVertical(input));
+    }
+    
+    /**
+     * this method returns the new string same as input string 
+     * but only one character at a line.
+     * @param input input string
+     * @return new String contains only 1 character at a line
+     */
+    public static String displayVertical(String input) {
+        String str = "";
+        
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (!(ch == ' ' || ch == '\t')) {
+                str += ch + "\n";
+            }
+        }
+
+        return str;
+    }
+}
